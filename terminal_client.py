90b2c7c7c5b8a8da5e6fd7e87ea8309dd7191516
liang <liang.huang@dci-student.org>
2025-02-18 14:40:59 +0100

    added logic to add questions and answers

--- terminal_client.py
+++ terminal_client.py
@@ -31,9 +31,10 @@ class TerminalClient(QuizClient):
             "2": self.get_set_topic,
             "3": self.set_difficulty_level,
             "4": self.add_new_topic,
-            # "5": self.add_new_question,
-            # "6": self.update_existing_question,
-            # "7": self.delete_question,
+            "5": self.add_new_question,
+            "6": self.add_answers_to_question,
+            # "7": self.update_existing_question,
+            # "8": self.delete_question,
         }
 
         self.clear_terminal()
@@ -45,6 +46,8 @@ class TerminalClient(QuizClient):
             print("2. View and set quiz topic.")
             print("3. Set difficulty level.")
             print("4. Add new topic")
+            print("5. Add new question")
+            print("6. Add answers to a question")
             print("0. Exit\n")
             choice: str = input("Enter your choice: ")
 
@@ -92,9 +95,14 @@ class TerminalClient(QuizClient):
             input("Press Enter to continue...")
             self.clear_terminal()
 
-        print(f"You have {correct} out of {len(questions)} correct.")
-        input("Press Enter to exit...")
-        self.clear_terminal()
+        if len(questions) > 0:
+            print(f"You have {correct} out of {len(questions)} correct.")
+            input("Press Enter to exit...")
+            self.clear_terminal()
+        else:
+            print("\nNo questions available for this quiz.")
+            input("Press Enter to exit...")
+            self.clear_terminal()
 
     def get_set_topic(self) -> None:
         """Set the topic for the quiz."""
@@ -141,7 +149,7 @@ class TerminalClient(QuizClient):
                 self.difficulty_level = DifficultyLevel.HARD
                 select_difficulty: bool = False
 
-        print("Difficulty level set to:", self.difficulty_level)
+        print("Difficulty level set to: ", self.difficulty_level)
         input("Press Enter to continue...")
         self.clear_terminal()
 
@@ -160,3 +168,79 @@ class TerminalClient(QuizClient):
                 adding_topic = False
         input("Press Enter to continue...")
         self.clear_terminal()
+
+    def add_new_question(self) -> None:
+        """Add a new question to the quiz."""
+        self.clear_terminal()
+        print("Adding question: ")
+        print("---------------------")
+        topics = self.get_topics()
+        adding_question: bool = True
+        print(topics)
+
+        topic_name = input(
+            "Enter the question's topic name (case sensitive): ",
+        )
+
+        while adding_question:
+            if not topic_name:
+                adding_question = False
+            elif topic_name not in topics:
+                print(f"Topic '{topic_name}' does not exist.")
+                input("Press Enter to continue...")
+                self.clear_terminal()
+                adding_question = False
+            else:
+                question = input("Enter the question to add: ")
+                if not question:
+                    adding_question = False
+                else:
+                    self.add_question(topic_name, question)
+                    print("\nQuestion added successfully.")
+                    print("\nPress Enter to continue...")
+                    self.clear_terminal()
+                    adding_question = False
+
+    def add_answers_to_question(self) -> None:
+        """Add answers to a specific question."""
+        self.clear_terminal()
+        adding_answer: bool = True
+
+        sub_command = {
+            "1": self.list_all_questions,
+            "2": self.add_answers_to_question,
+            "3": exit,
+        }
+
+        print("Adding answers to a specific question:")
+        print("-------------------\n")
+        print("1. List all questions")
+        print("2. Add answers to a specific question")
+        print("3. Exit\n")
+        option: str = input("Please select an option: ")
+        sub_command[option]()
+
+        while adding_answer:
+            question_id = input(
+                "\nEnter the id of the question to add answers: ",
+            )
+            answer: str = input("Enter the answer to add: ")
+            is_correct: bool = input(
+                "Is the answer correct? (True/False): ",
+            )
+            if not question_id:
+                adding_answer = False
+            else:
+                self.add_answers(question_id, answer, is_correct)
+                adding_answer = False
+        print("\nAnswer added successfully.")
+        input("Press Enter to continue...")
+        self.clear_terminal()
+
+    def list_all_questions(self) -> None:
+        """List all questions and its id."""
+        self.clear_terminal()
+        available_questions = self.get_available_questions()
+        print("Available Questions:\n")
+        for index, question in enumerate(available_questions):
+            print(f"{index + 1}. {question['question']}")
